11b1982f70b843ee35b8efd7284252cb4cf7fdb4
Tetiana.Chernova@enghouse.com <Tetiana.Chernova@enghouse.com>
2023-02-08 21:38:40 +0100

    TROUTBOX-1: add library skeleton

---
+++ src/main/java/com/chernova/transactional/outbox/annotation/WithTransactionalOutbox.java
@@ -0,0 +1,11 @@
+package com.chernova.transactional.outbox.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface WithTransactionalOutbox {
+}
---
+++ src/main/java/com/chernova/transactional/outbox/aspect/OutboxAspect.java
@@ -0,0 +1,27 @@
+package com.chernova.transactional.outbox.aspect;
+
+import com.chernova.transactional.outbox.annotation.WithTransactionalOutbox;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+@Slf4j
+public class OutboxAspect {
+
+	public OutboxAspect() {
+		System.out.println("in non arguments constructor in aspect class");
+	}
+
+	@Around("@annotation(withTransactionalOutboxAnnotation)")
+	public Object withTransactionalOutbox(ProceedingJoinPoint joinPoint, WithTransactionalOutbox withTransactionalOutboxAnnotation)
+			throws Throwable {
+		System.out.println("Triggered join point with withTransactionalOutbox annotation");
+		Object result = joinPoint.proceed();
+		System.out.println("After proceeding joinPoint");
+		return result;
+	}
+}
